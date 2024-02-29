ac2d3d4e2974baeb747d43a7fc1b6e7a1e550ea1
magab0nd <mahammadea@code.edu.az>
2024-02-29 06:26:14 -0800

    Problem Solved

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿#region Task 1
+
+class Account()
+{
+    string Username;
+    string Password;
+
+    public void Login()
+    {
+        if (Username == "Admin" || Password == "AdminEA18yr@")
+        {
+            Console.WriteLine("Ugurlu giris");
+            return;
+        }
+        Console.WriteLine("Username ve ya parol dogru deyil");
+    }
+}
+
+#endregion
+
+#region Task 2
+
+class User()
+{
+    string FirstName;
+    string LastName;
+    string Email;
+    string Password;
+    DateTime Birthday;
+    bool Gender;
+
+    void Register()
+    {
+        if (!string.IsNullOrEmpty(Password) || !string.IsNullOrEmpty(Email)) 
+        {
+            Console.WriteLine("Reqistrasiya ugurludur!");
+            return;
+        }
+        Console.WriteLine("Ugursuz");
+    }
+}
+
+#endregion
